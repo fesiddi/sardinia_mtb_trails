bb2eb1674503c3419d85c015a54b07c179912337
fesiddi <53375680+fesiddi@users.noreply.github.com>
2024-04-03 01:26:19 +0200

    Refactor CleanedSegment class to use object properties instead of dictionary access

--- app/models/CleanedSegment.py
+++ app/models/CleanedSegment.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, Field
 from typing import Optional
-from app.models.Segment import Segment
+from app.models.Segment import Segment, Map, LocalLegend
 
 
 class CleanedSegment(BaseModel):
@@ -11,29 +11,29 @@ class CleanedSegment(BaseModel):
     start_lng: Optional[float]
     end_lat: Optional[float]
     end_lng: Optional[float]
-    local_legend: Optional[dict]
+    local_legend: Optional[LocalLegend]
     star_count: int
     effort_count: int
     athlete_count: int
     kom: Optional[str]
-    map: dict
+    map: Map
     polyline: Optional[str]
 
     @classmethod
     def from_segment(cls, segment: Segment):
         return cls(
-            name=segment.get("name"),
-            id=segment.get("id"),
-            average_grade=segment.get("average_grade"),
-            start_lat=segment.get("start_latlng", [None, None])[0],
-            start_lng=segment.get("start_latlng", [None, None])[1],
-            end_lat=segment.get("end_latlng", [None, None])[0],
-            end_lng=segment.get("end_latlng", [None, None])[1],
-            local_legend=segment.get("local_legend"),
-            star_count=segment.get("star_count"),
-            effort_count=segment.get("effort_count"),
-            athlete_count=segment.get("athlete_count"),
-            kom=segment.get("xoms", {}).get("kom"),
-            map=segment.get("map"),
-            polyline=segment.get("map", {}).get("polyline"),
+            name=segment.name,
+            id=segment.id,
+            average_grade=segment.average_grade,
+            start_lat=segment.start_latlng.lat,
+            start_lng=segment.start_latlng.lng,
+            end_lat=segment.end_latlng.lat,
+            end_lng=segment.end_latlng.lng,
+            local_legend=segment.local_legend,
+            star_count=segment.star_count,
+            effort_count=segment.effort_count,
+            athlete_count=segment.athlete_count,
+            kom=segment.xoms.kom,
+            map=segment.map,
+            polyline=segment.map.polyline,
         )
