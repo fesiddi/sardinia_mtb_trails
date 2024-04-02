bd027832af18c1a61c456ef870be905c8a0213ab
fesiddi <53375680+fesiddi@users.noreply.github.com>
2024-04-03 01:15:30 +0200

    Add Instagram links to trail areas

--- app/segments_data/trail_areas_data.py
+++ app/segments_data/trail_areas_data.py
@@ -10,6 +10,7 @@ trail_areas_data = [
             {"name": "Carsten", "strava_id": "16631034"},
             {"name": "Gabriele", "strava_id": "13565577"},
         ],
+        "instagram": ["https://www.instagram.com/merenderosaho/"]
     },
     {
         "name": "Baunei",
@@ -21,6 +22,7 @@ trail_areas_data = [
         "local_riders": [
             {"name": "Efisio", "strava_id": "9869628"},
         ],
+        "instagram": ["https://www.instagram.com/efi__co/"]
     },
     {
         "name": "Capoterra",
@@ -32,6 +34,7 @@ trail_areas_data = [
         "local_riders": [
             {"name": "Matteo", "strava_id": "6900592"},
         ],
+        "instagram": ["https://www.instagram.com/capotrail/"]
     },
     {
         "name": "Carbonia",
@@ -43,6 +46,7 @@ trail_areas_data = [
         "local_riders": [
             {"name": "Davide", "strava_id": "8573339"},
         ],
+        "instagram": ["https://www.instagram.com/spakkaruote/"]
     },
     {
         "name": "Gavoi",
@@ -54,6 +58,7 @@ trail_areas_data = [
         "local_riders": [
             {"name": "Giuseppe", "strava_id": "7359860"},
         ],
+        "instagram": ["https://www.instagram.com/bikingavoi/"]
     },
     {
         "name": "Monte Arci",
@@ -65,6 +70,7 @@ trail_areas_data = [
         "local_riders": [
             {"name": "Fabrizio", "strava_id": "4488711"},
         ],
+        "instagram": ["https://www.instagram.com/anonimatrailbuilding_/"]
     },
     {
         "name": "Nuoro",
@@ -76,6 +82,7 @@ trail_areas_data = [
         "local_riders": [
             {"name": "Francesco", "strava_id": "13846867"},
         ],
+        "instagram": ["https://www.instagram.com/bikando_shuttle_nuoro/", "https://www.instagram.com/biketeamdemurtasnuoro/"]
     },
     {   
         "name": "Olbia",
@@ -87,5 +94,7 @@ trail_areas_data = [
         "local_riders": [
             {"name": "Mirko", "strava_id": "20292657"},
         ],
+        "instagram": ["https://www.instagram.com/olbiaracingteam/"]
+
     },
 ]
