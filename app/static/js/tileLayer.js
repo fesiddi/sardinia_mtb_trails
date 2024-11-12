9f0bd3e1c8c3eccb080943a9a44f7bf6514fa628
fesiddi <sfdev25@gmail.com>
2024-11-12 14:40:14 +0100

    Replace tile server URL with OpenStreetMap source in tileLayer.js

--- app/static/js/tileLayer.js
+++ app/static/js/tileLayer.js
@@ -1,7 +1,4 @@
-const TILE_SERVER_URL =
-    'https://tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=52c2b5d8de3e4f1ab03dc1e108cd1a3d';
-
 export const createTileLayer = () =>
     new ol.layer.Tile({
-        source: new ol.source.XYZ({ url: TILE_SERVER_URL }),
+        source: new ol.source.OSM(),
     });
