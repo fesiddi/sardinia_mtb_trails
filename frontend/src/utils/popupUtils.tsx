4eb7c15aea4b5ba78a98a573c3b6afea88c02e1e
fesiddi <sfdev25@gmail.com>
2024-11-27 16:36:08 +0100

    Refactor popup functionality in TrailAreaMap: modularize popup element creation and enhance click handling for better readability and maintainability

--- frontend/src/utils/popupUtils.tsx
+++ frontend/src/utils/popupUtils.tsx
@@ -7,12 +7,8 @@ import styles from '../components/TrailAreaMap.module.css';
 export const initializePopup = (mapInstance: Map) => {
     console.log('Initializing popup');
 
-    const popupElement = document.createElement('div');
-    popupElement.id = 'popup';
-    popupElement.className = styles.olPopup;
-
+    const popupElement = createPopupElement();
     document.body.appendChild(popupElement);
-
 
     const popup = new Overlay({
         element: popupElement,
@@ -20,106 +16,130 @@ export const initializePopup = (mapInstance: Map) => {
         stopEvent: false,
         offset: [0, -10],
     });
+
     mapInstance.addOverlay(popup);
     console.log('Popup overlay added to map');
 
-    const closer = document.createElement('a');
-    closer.href = '#';
-    closer.id = 'popup-closer';
-    closer.className = styles.olPopupCloser;
-    closer.innerHTML = '✖';
+    const closer = createCloserElement(popup);
     popupElement.appendChild(closer);
 
-    const content = document.createElement('div');
-    content.id = 'popup-content';
+    const content = createContentElement();
     popupElement.appendChild(content);
 
-    closer.onclick = function () {
+    mapInstance.on('click', (evt) =>
+        handleMapClick(evt, mapInstance, popup, content)
+    );
+};
+
+
+function createPopupElement() {
+    const popupElement = document.createElement('div');
+    popupElement.className = styles.olPopup;
+    return popupElement;
+}
+
+function createCloserElement(popup: Overlay) {
+    const closer = document.createElement('a');
+    closer.href = '#';
+    closer.className = styles.olPopupCloser;
+    closer.innerHTML = '✖';
+    closer.onclick = () => {
         console.log('Popup closer clicked');
         popup.setPosition(undefined);
         closer.blur();
         return false;
     };
+    return closer;
+}
 
-    mapInstance.on('click', function (evt) {
-        console.log('Map clicked at', evt.pixel);
-        const feature = mapInstance.forEachFeatureAtPixel(
-            evt.pixel,
-            function (feature) {
-                return feature;
-            }
-        );
+function createContentElement() {
+    const content = document.createElement('div');
+    return content;
+}
 
-        if (feature) {
-            console.log('Feature clicked:', feature);
-            const featureProperties = feature.getProperties();
-            console.log('Feature properties: ', featureProperties);
-            const geometry = feature.getGeometry() as Point;
-            const coordinates = geometry.getCoordinates();
-            showPopup(coordinates, feature);
+function handleMapClick(
+    evt: any,
+    mapInstance: Map,
+    popup: Overlay,
+    content: HTMLElement
+) {
+    console.log('Map clicked at', evt.pixel);
+    const feature = mapInstance.forEachFeatureAtPixel(
+        evt.pixel,
+        (feature) => feature
+    );
 
-        } else {
-            console.log('No feature clicked');
-        }
-    });
-
-    function showPopup(coordinates: number[], feature: any) {
-        console.log('Showing popup at coordinates:', coordinates);
-        const content = document.getElementById('popup-content');
-        if (content) {
-            if (feature.get('type') === 'trailBaseMarker') {
-                content.innerHTML = generateTrailBaseContent(
-                    feature,
-                    coordinates
-                );
-                popup.setPosition(coordinates);
-                console.log('Popup position set to:', coordinates);
-            } else if (feature.get('type') === 'segment') {
-                console.log('content is a segment');
-                content.innerHTML = generateSegmentContent(feature);
-                const segment = feature.get('segment');
-                const startLat = segment.start_lat;
-                const startLon = segment.start_lng;
-                popup.setPosition(fromLonLat([startLon, startLat]));
-                console.log('Popup position set to:', startLat, startLon);
-            }
-        } else {
-            console.error('Popup content element not found');
-            throw new Error('Popup content element not found');
-        }
+    if (feature) {
+        console.log('Feature clicked:', feature);
+        const featureProperties = feature.getProperties();
+        console.log('Feature properties: ', featureProperties);
+        const geometry = feature.getGeometry() as Point;
+        const coordinates = geometry.getCoordinates();
+        showPopup(popup, coordinates, feature, content);
+    } else {
+        console.log('No feature clicked');
     }
+}
 
-    function generateTrailBaseContent(feature: any, coordinates: number[]) {
-        const properties = feature.getProperties();
-        console.log('Trail Base properties: ', properties);
-        const [longitude, latitude] = toLonLat(coordinates);
-        const googleMapsLink = `https://www.google.com/maps?q=${latitude},${longitude}`;
-        return `
-            <h4><strong>${properties.name || 'Trail Base Marker'}</strong></h4>
-            <a href="${googleMapsLink}" target="_blank">View in Maps</a>
-        `;
-    }
-
-    function generateSegmentContent(feature: any) {
-        const segment = feature.get('segment');
-        console.log('Segment content: ', segment);
-        return `
-        <h4><strong>${segment.alt_name}</strong></h4>
-        <p><strong>KOM: </strong>${segment.kom}</p>
-        <p><strong>Athletes Count: </strong>${segment.athlete_count}</p>
-        ${
-            segment.local_legend &&
-            segment.local_legend.title &&
-            segment.local_legend.athlete_id
-                ? `<p><strong>Local Legend: </strong><a href="https://www.strava.com/athletes/${segment.local_legend.athlete_id}">${segment.local_legend.title}</a></p>`
-                : ''
+function showPopup(
+    popup: Overlay,
+    coordinates: number[],
+    feature: any,
+    content: HTMLElement
+) {
+    if (content) {
+        if (feature.get('type') === 'trailBaseMarker') {
+            content.innerHTML = generateTrailBaseContent(
+                feature,
+                coordinates
+            );
+            popup.setPosition(coordinates);
+            console.log('Popup position set to:', coordinates);
+        } else if (feature.get('type') === 'segment') {
+            console.log('content is a segment');
+            content.innerHTML = generateSegmentContent(feature);
+            const segment = feature.get('segment');
+            const startLat = segment.start_lat;
+            const startLon = segment.start_lng;
+            popup.setPosition(fromLonLat([startLon, startLat]));
+            console.log('Popup position set to:', startLat, startLon);
         }
-        <p>${
-            segment.local_legend ? segment.local_legend.effort_description : ''
-        }</p>
-        <p><a href="https://www.strava.com/segments/${
-            segment.id
-        }">View Segment on Strava</a></p>
+    } else {
+        console.error('Popup content element not found');
+        throw new Error('Popup content element not found');
+    }
+}
+
+function generateTrailBaseContent(feature: any, coordinates: number[]) {
+    const properties = feature.getProperties();
+    console.log('Trail Base properties: ', properties);
+    const [longitude, latitude] = toLonLat(coordinates);
+    const googleMapsLink = `https://www.google.com/maps?q=${latitude},${longitude}`;
+    return `
+        <h4><strong>${properties.name || 'Trail Base Marker'}</strong></h4>
+        <a href="${googleMapsLink}" target="_blank">View in Maps</a>
     `;
+}
+
+function generateSegmentContent(feature: any) {
+    const segment = feature.get('segment');
+    console.log('Segment content: ', segment);
+    return `
+    <h4><strong>${segment.alt_name}</strong></h4>
+    <p><strong>KOM: </strong>${segment.kom}</p>
+    <p><strong>Athletes Count: </strong>${segment.athlete_count}</p>
+    ${
+        segment.local_legend &&
+        segment.local_legend.title &&
+        segment.local_legend.athlete_id
+            ? `<p><strong>Local Legend: </strong><a href="https://www.strava.com/athletes/${segment.local_legend.athlete_id}">${segment.local_legend.title}</a></p>`
+            : ''
     }
-};
+    <p>${
+        segment.local_legend ? segment.local_legend.effort_description : ''
+    }</p>
+    <p><a href="https://www.strava.com/segments/${
+        segment.id
+    }">View Segment on Strava</a></p>
+`;
+}
